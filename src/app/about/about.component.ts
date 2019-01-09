c77bc2c933471176f283f69443f9899c5bba8c93
Justin Weiskittel <justinweiskittel@gmail.com>
2019-01-09 15:14:59 -0500

    footer added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const img = document.querySelector('img');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 1) {
+        img.style.width = '4.25vw';
+        img.style.background = 'transparent';
+        img.style.marginLeft = '-2.125vw';
+      } else {
+        img.style.width = '11vw';
+        img.style.background = 'white';
+        img.style.marginLeft = '-5.5vw';
+      }
+    });
   }
 
 }
