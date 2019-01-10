34aac4e4e5d251de6a3713f2e12ea479007547b7
Justin Weiskittel <justinweiskittel@gmail.com>
2019-01-10 13:42:18 -0500

    navbar work

--- src/app/wood/wood.component.ts
+++ src/app/wood/wood.component.ts
@@ -10,13 +10,24 @@ export class WoodComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const shadow = document.querySelector('div');
     const img = document.querySelector('img');
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 1) {
+        shadow.style.height = '3.3vw';
+        shadow.style.width = '4.25vw';
+        shadow.style.marginLeft = '-2.125vw';
+        shadow.style.background = 'transparent';
+        shadow.style.boxShadow = 'none';
         img.style.width = '4.25vw';
         img.style.background = 'transparent';
         img.style.marginLeft = '-2.125vw';
       } else {
+        shadow.style.height = '8.67vw';
+        shadow.style.width = '11.04vw';
+        shadow.style.marginLeft = '-5.52vw';
+        shadow.style.background = 'white';
+        shadow.style.boxShadow = '2px 2px 10px black, -2px 0px 10px black';
         img.style.width = '11vw';
         img.style.background = 'white';
         img.style.marginLeft = '-5.5vw';
