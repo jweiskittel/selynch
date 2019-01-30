f513f8a582aef0b5d786e6685c991c56a056a81a
Justin Weiskittel <justinweiskittel@gmail.com>
2019-01-30 02:11:17 -0500

    deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +30,6 @@ import { OwlModule } from 'ngx-owl-carousel';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     OwlModule
   ],
   providers: [],
