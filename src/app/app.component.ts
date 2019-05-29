5784654e0c4106f0a5130d045c06ceeb5164a7c6
Justin Weiskittel <justinweiskittel@gmail.com>
2019-05-29 10:51:18 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'selynch';
-
-  constructor(private route: Router) {}
-
-  ngOnInit() {
-    console.log(this.route.url);
-  }
+export class AppComponent {
 }
